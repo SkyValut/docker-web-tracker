b5a27be6825cf7916038b8717f68f1e98420653b
SkyValut <skyeditor23@gmail.com>
2022-06-23 11:29:40 +0530

    project

---
+++ red.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/helpline')
+def help():
+    return render_template('help.html')
+
+@app.route('/track', methods=['POST'])
+def track():
+    if request.method == 'POST':
+        url = request.form['user_url']
+        resp = requests.get(url)
+        redirect_ulr = resp.history
+        return render_template('track.html', endurl = resp, redirect = redirect_ulr)
+
+if __name__== "__main__":
+  app.run(debug=True)
